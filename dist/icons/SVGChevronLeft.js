1341574011f3de739078328fed2c21caa2e159cf
Amine Amine <amineamine19961996@gmail.com>
2021-10-01 20:07:10 +0300

    TEST: react import

--- dist/icons/SVGChevronLeft.js
+++ dist/icons/SVGChevronLeft.js
@@ -5,16 +5,20 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = SVGChevronLeft;
 
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 function SVGChevronLeft(props) {
   var _props$color;
 
-  return /*#__PURE__*/React.createElement("svg", {
+  return /*#__PURE__*/_react.default.createElement("svg", {
     xmlns: "http://www.w3.org/2000/svg",
     width: "15",
     height: "15",
     fill: (_props$color = props.color) !== null && _props$color !== void 0 ? _props$color : "currentColor",
     viewBox: "0 0 24 24"
-  }, /*#__PURE__*/React.createElement("path", {
+  }, /*#__PURE__*/_react.default.createElement("path", {
     d: "M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z"
   }));
 }
--- src/lib/icons/SVGChevronLeft.js
+++ src/lib/icons/SVGChevronLeft.js
@@ -1,3 +1,4 @@
+import React from "react";
 export default function SVGChevronLeft(props) {
   return (
     <svg

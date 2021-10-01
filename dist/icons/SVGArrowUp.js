888bdb192e894ad5898a5330490e33d45f843fe7
Amine Amine <amineamine19961996@gmail.com>
2021-10-01 20:11:38 +0300

    ADD: imports to build

--- dist/icons/SVGArrowUp.js
+++ dist/icons/SVGArrowUp.js
@@ -5,17 +5,21 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = SVGArrowUp;
 
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 function SVGArrowUp(props) {
   var _props$color;
 
-  return /*#__PURE__*/React.createElement("svg", {
+  return /*#__PURE__*/_react.default.createElement("svg", {
     xmlns: "http://www.w3.org/2000/svg",
     width: "16",
     height: "16",
     fill: (_props$color = props.color) !== null && _props$color !== void 0 ? _props$color : "currentColor",
     className: "bi bi-arrow-up",
     viewBox: "0 0 16 16"
-  }, /*#__PURE__*/React.createElement("path", {
+  }, /*#__PURE__*/_react.default.createElement("path", {
     fillRule: "evenodd",
     d: "M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"
   }));
